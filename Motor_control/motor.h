0b305dffddea8180de61805f5ab0c05f71ddff60
daduranka <116219580+daduranka@users.noreply.github.com>
2023-04-21 15:37:54 -0600

    cleaning up code

--- Motor_control/motor.h
+++ Motor_control/motor.h
@@ -10,15 +10,6 @@
  *  Global Variable and Type Declarations
  *  -------------------------------------------------------------------------------------------------------------
  */
-extern volatile int16_t error_integral;    // Integrated error signal
-extern volatile uint8_t duty_cycle;    // Output PWM duty cycle
-extern volatile int16_t target_rpm;    // Desired speed target
-extern volatile int16_t motor_speed;   // Measured motor speed
-extern volatile int8_t adc_value;      // ADC measured motor current
-extern volatile int16_t error;         // Speed error signal
-extern volatile uint8_t Kp;            // Proportional gain
-extern volatile uint8_t Ki;            // Integral gain
-
 extern volatile int16_t left_error_integral;    // Integrated error signal
 extern volatile uint8_t left_duty_cycle;    // Output PWM duty cycle
 extern volatile int16_t left_target_rpm;    // Desired speed target
@@ -42,17 +33,14 @@ extern volatile uint8_t right_Ki;            // Integral gain
  */
 
 // Sets up the entire motor drive system
-void motor_init(void);
 void left_motor_init(void);
 void right_motor_init(void);
 
 // Set the duty cycle of the PWM, accepts (0-100)
-void pwm_setDutyCycle(uint8_t duty);
 void left_setDutyCycle(uint8_t left_duty);
 void right_setDutyCycle(uint8_t right_duty);
 
 // PI control code is called within a timer interrupt
-void PI_update(void);
 void left_PI_update(void);
 void right_PI_update(void);
 
@@ -62,17 +50,14 @@ void right_PI_update(void);
  */
 
 // Sets up the PWM and direction signals to drive the H-Bridge
-void pwm_init(void);
 void left_pwm_init(void);
 void right_pwm_init(void);
 
 // Sets up encoder interface to read motor speed
-void encoder_init(void);
 void left_encoder_init(void);
 void right_encoder_init(void);
 
 // Sets up ADC to measure motor current
-void ADC_init(void);
 void left_ADC_init(void);
 void right_ADC_init(void);
 
