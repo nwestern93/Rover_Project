b709817132dd7351674f6f68e6273cd24eead2df
daduranka <116219580+daduranka@users.noreply.github.com>
2023-04-26 12:43:18 -0600

    added motor control to src

--- Src/main.c
+++ Src/main.c
@@ -21,7 +21,7 @@
 #include "distance_front.h"
 #include "BT_usart.h"
 #include "color_sensor_set.h"
-#include "i3g_IR.h"
+#include "motor.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -43,7 +43,6 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc;
 
 /* USER CODE BEGIN PV */
 
@@ -51,8 +50,6 @@ ADC_HandleTypeDef hadc;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_ADC_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -61,21 +58,6 @@ static void MX_ADC_Init(void);
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
-
-void LED_Init(void){
-	RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;
-	//Configuring red, blue, orange, and green LEDs to Output mode
-	GPIOC -> MODER &= ~((1<<19) | (1<<18) | (1<<17) | (1<<16) | (1<<15) | (1<<14) | (1<<13) | (1<<12));
-	GPIOC -> MODER |= (1<<18) | (1<<16) | (1<<14) | (1<<12);
-
-	//Setting red, blue, orange, and green LEDs to no pull up or pull down mode
-	GPIOC -> PUPDR &= ~((1<<19) | (1<<18) | (1<<17) | (1<<16) | (1<<15) | (1<<14) | (1<<13) | (1<<12));
-	
-	//Setting red, blue, orange, and green LEDs to low speed
-	GPIOC -> OSPEEDR &= ~((1<<19) | (1<<18) | (1<<17) |(1<<16) | (1<<15) | (1<<14) | (1<<13) | (1<<12));
-
-
-}
 
 /**
   * @brief  The application entry point.
@@ -91,6 +73,7 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
@@ -99,22 +82,14 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_ADC_Init();
-  /* USER CODE BEGIN 2 */
 	USART3_Init();
 	BT_usart_init();
-	LED_Init();
+	//LED_Init();
   color_sensor_init();
-	
-	HAL_ADC_Start(&hadc);
-	
-  /* USER CODE END 2 */
-	char* message_start = "start";
+	right_motor_init();
+	left_motor_init();
+  /* USER CODE END SysInit */
+char* message_start = "start";
 	char* message_errror = "Error";
 	char* message_newCMD = "New Command:";
 	char* newline = "\n";
@@ -153,18 +128,20 @@ int main(void)
 			BT_usart_transmit_message(newline);
 		
 		} else if (opCode == '3'){	//Ir sensor 1
-			int distraw=0;
-			distraw = ir_1_getDist();
-			Str = itoa(distraw, buffer, 10);
-			BT_usart_transmit_message("Left: ");
-			BT_usart_transmit_message(Str);
-			BT_usart_transmit_message(newline);
-			
-			distraw = ir_2_getDist();
-			Str = itoa(distraw, buffer, 10);
-			BT_usart_transmit_message("Right: ");
-			BT_usart_transmit_message(Str);
-			BT_usart_transmit_message(newline);
+//			int distraw=0;
+//			distraw = ir_1_getDist();
+//			Str = itoa(distraw, buffer, 10);
+//			BT_usart_transmit_message("Left: ");
+//			BT_usart_transmit_message(Str);
+//			BT_usart_transmit_message(newline);
+//			
+//			distraw = ir_2_getDist();
+//			Str = itoa(distraw, buffer, 10);
+//			BT_usart_transmit_message("Right: ");
+//			BT_usart_transmit_message(Str);
+//			BT_usart_transmit_message(newline);
+		} else if (opCode == '4'){ // motor commands start?
+			turn_left();
 		} else{
 			BT_usart_transmit_message(message_errror);
 			BT_usart_transmit_message(newline);
@@ -188,16 +165,15 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI14;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.HSI14CalibrationValue = 16;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
+
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
@@ -210,78 +186,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief ADC Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_ADC_Init(void)
-{
-
-  /* USER CODE BEGIN ADC_Init 0 */
-
-  /* USER CODE END ADC_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC_Init 1 */
-
-  /* USER CODE END ADC_Init 1 */
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc.Instance = ADC1;
-  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-  hadc.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
-  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  hadc.Init.LowPowerAutoWait = DISABLE;
-  hadc.Init.LowPowerAutoPowerOff = DISABLE;
-  hadc.Init.ContinuousConvMode = DISABLE;
-  hadc.Init.DiscontinuousConvMode = DISABLE;
-  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc.Init.DMAContinuousRequests = DISABLE;
-  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
-  if (HAL_ADC_Init(&hadc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure for the selected ADC regular channel to be converted.
-  */
-  sConfig.Channel = ADC_CHANNEL_10;
-  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Configure for the selected ADC regular channel to be converted.
-  */
-  sConfig.Channel = ADC_CHANNEL_11;
-  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC_Init 2 */
-
-  /* USER CODE END ADC_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-
 }
 
 /* USER CODE BEGIN 4 */
@@ -319,4 +223,3 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
