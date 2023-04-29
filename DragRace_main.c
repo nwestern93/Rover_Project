630e72dccbb70237623e19f212660aa8557431ad
Dmitry Panin <126808819+Dima5877@users.noreply.github.com>
2023-04-28 20:39:18 -0600

    Update DragRace_main.c

--- DragRace_main.c
+++ DragRace_main.c
@@ -1,6 +1,7 @@
 /*
 
 TASK TO COMPLETE:
+
 Drag Race – Our rover will attempt to drive as fast as possible to a finish line and stop as
 close to the finish line as possible, again, without us being able to watch the rover while
 it drives.
@@ -41,6 +42,28 @@ int main(void)
   while (1)
   {
    
+    //set left right motor to 100% forward
+    
+     //set right motor to 100% forward
+    
+    //if (direction of rover changes too fast to the left)     Use I2C GYRO?
+    {
+      //slow down right wheel until direction same as original
+    }
+    
+    //else if (direction of rover changes too fast to the right)
+    {
+      //slow down left wheel until direction same as original
+    }
+    
+    //else if (too much slipping of one wheel occurs)
+    {
+      //slow down that wheel to attempt to correct straight course
+    }
+    
+    
+    
+    
   }
 
 }
