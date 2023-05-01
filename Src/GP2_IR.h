f6a6c80e22f0cf077fdcd41e574c225689b41d28
KourFrost <j4c0bh3rrm4nn@gmail.com>
2023-05-01 16:50:33 -0600

    Update GP2_IR.h
    updated to pull

--- Src/GP2_IR.h
+++ Src/GP2_IR.h
@@ -2,8 +2,8 @@
 #include "stm32f0xx.h"
 #include "main.h"
 
-void Error_Handler(void);
-
+void ir_1_init(void);
 int16_t ir_1_getDist(void);
 int16_t ir_2_getDist(void);
 void ir_getDist(void);
+void ADC_Enable(void);
