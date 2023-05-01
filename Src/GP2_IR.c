7c2e984332a1f5aabf384561a13d67ccc66b6bec
KourFrost <j4c0bh3rrm4nn@gmail.com>
2023-05-01 16:49:56 -0600

    Update GP2_IR.c
    updated the ADC to pull

--- Src/GP2_IR.c
+++ Src/GP2_IR.c
@@ -1,25 +1,94 @@
+
 #include "GP2_IR.h"
 
 int16_t  ir_1;
 int16_t	 ir_2;
 
+void calibration(void){
+// from page 237 in perph manual
+//1. Ensure that ADEN = 0 and DMAEN = 0.
+//2. Set ADCAL = 1.
+//3. Wait until ADCAL = 0.
+//4. The calibration factor can be read from bits 6:0 of ADC_DR
+	if ((ADC1->CR & ADC_CR_ADEN) != 0) /* (1) */
+	{
+	ADC1->CR |= ADC_CR_ADDIS; /* (2) */
+	}
+	while ((ADC1->CR & ADC_CR_ADEN) != 0)
+	{
+	/* For robust implementation, add here time-out management */
+	}
+	ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN; /* (3) */
+	ADC1->CR |= ADC_CR_ADCAL; /* (4) */
+	while ((ADC1->CR & ADC_CR_ADCAL) != 0) /* (5) */
+	{
+	/* For robust implementation, add here time-out management */
+	}
+}
+
+void ADC_Enable(void){
+		/* (1) Ensure that ADRDY = 0 */
+	/* (2) Clear ADRDY */
+	/* (3) Enable the ADC */
+	/* (4) Wait until ADC ready */
+	if ((ADC1->ISR & ADC_ISR_ADRDY) != 0) /* (1) */
+	{
+		ADC1->ISR |= ADC_ISR_ADRDY; /* (2) */
+	}
+	ADC1->CR |= ADC_CR_ADEN; /* (3) */
+	while ((ADC1->ISR & ADC_ISR_ADRDY) == 0) /* (4) */
+	{
+	/* For robust implementation, add here time-out management */
+	}
+}
+
+void ADC_Disable(void){
+	/* (1) Stop any ongoing conversion */
+	/* (2) Wait until ADSTP is reset by hardware i.e. conversion is stopped */
+	/* (3) Disable the ADC */
+	/* (4) Wait until the ADC is fully disabled */
+	ADC1->CR |= ADC_CR_ADSTP; /* (1) */
+	while ((ADC1->CR & ADC_CR_ADSTP) != 0) /* (2) */
+	{
+	/* For robust implementation, add here time-out management */
+	}
+	ADC1->CR |= ADC_CR_ADDIS; /* (3) */
+	while ((ADC1->CR & ADC_CR_ADEN) != 0) /* (4) */
+	{
+	/* For robust implementation, add here time-out management */
+	}
+}
+
 void ir_1_init(void){
+	
+	
 	RCC->AHBENR |= RCC_APB2ENR_ADC1EN;//enable ADC1 in RCC
-	GPIOC->MODER |= (3 << 0);//PC0 = ADC input to analog, no pull up/down (?default)
+	GPIOC->MODER |= (0xFF << 0);//PC0 = ADC input to analog, no pull up/down (?default)
+	//GPIOC->MODER |= (3 << 2);//PC1
 	//MX_ADC_Init(); //Initialize ADC. PC0 = ADC input (potentiometer middle pin, others to GND and 3V)
-	 //Calibration
-	ADC1->CR |= ADC_CR_ADDIS; //Set ADEN = 0;
-	ADC1->CR |= (1<<15);
-		//while ((ADC1->CR & ADC_CR_ADEN) != 0) {}
-	ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN; // Set DMAEN = 0
-	ADC1->CR |= ADC_CR_ADCAL; //Set ACAL = 1
-	while ((ADC1->CR & ADC_CR_ADCAL) != 0){} //Wait until ADCAL=0
-	
+  // Set 8-bit resolution
+  ADC1->CFGR1 |= (1 << 4);
+	  // Turn off hardware triggers
+  ADC1->CFGR1 &= ~((1 << 10) | (1 << 11));
+//	ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN; // Set DMAEN = 0
+//	ADC1->CHSELR |= (1<<10); //set ADC1 to channel 10, redundant with MX_ADC_Init()
+//	ADC1->CHSELR |= (1<<11);
+//	ADC1->CFGR1 &= ~(ADC_CFGR1_CONT); //set ADC1 to continuous sampling, redundant with MX_ADC_Init()
+//	//HAL_ADC_Start(&hadc);  //START ADC CONVERSION
+	calibration();
+
+  ADC1->CHSELR |= (1<<10); //set ADC1 to channel 10, redundant with MX_ADC_Init()
+	ADC1->CHSELR |= (1<<11);
+    
+  // Ensure ADSTP, ADSTART, ADDIS are = 0
+  ADC1->CR &= ~(1 << 4); //STP
+  ADC1->CR &= ~(1 << 2); //START
+  ADC1->CR &= ~(1 << 1); //DIS
+
+  ADC1->CR |= 0x1;
+
+  ADC1->CR |= 1 << 2;
 	ADC1->CR |= ADC_CR_ADEN; //Enable ADC
-	ADC1->CHSELR |= (1<<10); //set ADC1 to channel 10, redundant with MX_ADC_Init()
-	ADC1->CFGR1 |= ADC_CFGR1_CONT; //set ADC1 to continuous sampling, redundant with MX_ADC_Init()
-	//HAL_ADC_Start(&hadc);  //START ADC CONVERSION
-	
 }
 
 void ir_getDist(void){
@@ -30,11 +99,47 @@ void ir_getDist(void){
 }
 
 int16_t ir_1_getDist(void){
-	ir_getDist();
-	return ir_1;
+	
+	int16_t val = 0;
+		/* (1) Select HSI14 by writing 00 in CKMODE (reset value) */
+	/* (2) Select CHSEL0, CHSEL9, CHSEL10 andCHSEL17 for VRefInt */
+	/* (3) Select a sampling mode of 111 i.e. 239.5 ADC clk to be greater
+	than 17.1us */
+	/* (4) Wake-up the VREFINT (only for VBAT, Temp sensor and VRefInt) */
+	//ADC1->CFGR2 &= ~ADC_CFGR2_CKMODE; /* (1) */
+	ADC1->CHSELR = ADC_CHSELR_CHSEL10; /* (2) */
+	ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2; /* (3) */
+	ADC->CCR |= ADC_CCR_VREFEN; /* (4) */
+		/* Performs the AD conversion */
+	ADC1->CR |= ADC_CR_ADSTART; /* Start the ADC conversion */
+	while ((ADC1->ISR & ADC_ISR_EOC) == 0) /* Wait end of conversion */
+	{
+	/* For robust implementation, add here time-out management */
+	}
+	val = ADC1->DR; /* Store the ADC conversion result */
+	ADC1->CFGR1 ^= ADC_CFGR1_SCANDIR; /* Toggle the scan direction */
+	return val;
 }
 
 int16_t ir_2_getDist(void){
-	ir_getDist();
-	return ir_2;
+
+	int16_t val = 0;
+		/* (1) Select HSI14 by writing 00 in CKMODE (reset value) */
+	/* (2) Select CHSEL0, CHSEL9, CHSEL10 andCHSEL17 for VRefInt */
+	/* (3) Select a sampling mode of 111 i.e. 239.5 ADC clk to be greater
+	than 17.1us */
+	/* (4) Wake-up the VREFINT (only for VBAT, Temp sensor and VRefInt) */
+	//ADC1->CFGR2 &= ~ADC_CFGR2_CKMODE; /* (1) */
+	ADC1->CHSELR = ADC_CHSELR_CHSEL11; /* (2) */
+	ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2; /* (3) */
+	ADC->CCR |= ADC_CCR_VREFEN; /* (4) */
+		/* Performs the AD conversion */
+	ADC1->CR |= ADC_CR_ADSTART; /* Start the ADC conversion */
+	while ((ADC1->ISR & ADC_ISR_EOC) == 0) /* Wait end of conversion */
+	{
+	/* For robust implementation, add here time-out management */
+	}
+	val = ADC1->DR; /* Store the ADC conversion result */
+	ADC1->CFGR1 ^= ADC_CFGR1_SCANDIR; /* Toggle the scan direction */
+	return val;
 }
