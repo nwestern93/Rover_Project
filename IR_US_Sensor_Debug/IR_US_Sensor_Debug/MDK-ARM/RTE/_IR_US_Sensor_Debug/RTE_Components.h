c344f6d56c5a18c1d1ee8b6d7eaa6c9b65b2b62a
daduranka <116219580+daduranka@users.noreply.github.com>
2023-05-01 21:37:10 -0600

    building new project

---
+++ IR_US_Sensor_Debug/IR_US_Sensor_Debug/MDK-ARM/RTE/_IR_US_Sensor_Debug/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'IR_US_Sensor_Debug' 
+ * Target:  'IR_US_Sensor_Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
